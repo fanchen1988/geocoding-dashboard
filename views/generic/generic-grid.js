fff827ad3718787a08aede172323aa1c76586f70
Chen Fan <fanchen@Chens-MacBook-Pro.local>
2016-07-21 21:24:26 +0800

    Use customized field type to render chart link and highlight accuracy

--- views/generic/generic-grid.js
+++ views/generic/generic-grid.js
@@ -6,12 +6,32 @@ class GenericGrid extends GridLoader {
   }
 
   getFields() {
+    let urlFieldName = 'urlField';
+    let urlFieldConfig = {
+      itemTemplate: function(value, item) {
+        return $('<a href="/chart/' + item.countryCode.toString() + '">' + value.toString() + '</a>');
+      }
+    };
+    this.setCustomizedField(urlFieldConfig, urlFieldName);
+
+    let accuracyFieldName = 'accuracyField';
+    let accuracyFieldConfig = {
+      itemTemplate: function(value, item) {
+        let result = value;
+        if (value < item.goal) {
+          result = '<font color="red">' + value.toString() + '</font>';
+        }
+        return result;
+      }
+    };
+    this.setCustomizedField(accuracyFieldConfig, accuracyFieldName);
+
     return [
-      {name: 'country', title: "Country", type: "text", width: 40},
+      {name: 'country', title: "Country", type: urlFieldName, width: 40},
       {name: 'tier', title: "Tier", type: "number", width: 18},
       {name: 'version', title: "Version", type: "text", width: 25},
       {name: 'density', title: "Geo Density", type: "number", width: 40},
-      {name: 'accuracy', title: "Geo Accuracy", type: "number", width: 45},
+      {name: 'accuracy', title: "Geo Accuracy", type: accuracyFieldName, width: 45},
       {name: 'goal', title: "Goal", type: "number", width: 20},
       {name: 'comprehensiveness', title: "Geo Comprehensiveness", type: "number", width: 70},
       {name: 'geolevelDensity', title: "Geolevel Density", type: "number", width: 60},
