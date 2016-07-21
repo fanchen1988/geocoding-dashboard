d2bf30b48c091c05798e0db7eded1c167877b34d
Chen Fan <fanchen@Chens-MacBook-Pro.local>
2016-07-21 20:31:09 +0800

    Refactor geosummarizer so that front control what title to display

--- views/geosummarizer/geosummarizer-grid.js
+++ views/geosummarizer/geosummarizer-grid.js
@@ -7,14 +7,14 @@ class GeosummarizerGrid extends GridLoader {
 
   getFields() {
     return [
-      { name: "Country", type: "text", width: 30, validate: "required" },
-      { name: "Tier", type: "number", width: 20 },
-      { name: "Version", type: "text", width: 30 },
-      { name: "Geosummarizer Accuracy", type: "number", width: 100 },
-      { name: "Good Geosummarizer Rate", type: "number", width: 100 },
-      { name: "Ideal Input Rate", type: "number", width: 80 },
-      { name: "Ideal Input Standard Deviation", type: "number", width: 120 },
-      { name: "Input Accuracy", type: "number", width: 80 },
+      { name: 'country', title: "Country", type: "text", width: 30, validate: "required" },
+      { name: 'tier', title: "Tier", type: "number", width: 20 },
+      { name: 'version', title: "Version", type: "text", width: 30 },
+      { name: 'geosumAccuracy', title: "Geosummarizer Accuracy", type: "number", width: 100 },
+      { name: 'geosumRate', title: "Good Geosummarizer Rate", type: "number", width: 100 },
+      { name: 'inputRate', title: "Ideal Input Rate", type: "number", width: 80 },
+      { name: 'inputSD', title: "Ideal Input Standard Deviation", type: "number", width: 120 },
+      { name: 'inputAccuracy', title: "Input Accuracy", type: "number", width: 80 },
     ];
   }
 
