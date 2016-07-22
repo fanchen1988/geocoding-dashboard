17efe6405565dadb9763c46a3aae2bceca39a4b5
Chen Fan <fanchen@Chens-MacBook-Pro.local>
2016-07-22 15:51:24 +0800

    Add run button on page

--- views/geosummarizer/geosummarizer-grid.js
+++ views/geosummarizer/geosummarizer-grid.js
@@ -6,15 +6,25 @@ class GeosummarizerGrid extends GridLoader {
   }
 
   getFields() {
+    let runFieldName = 'runField';
+    let runFieldConfig = {
+      itemTemplate: function(value, item) {
+        let btnStyleClass = value === item.version ? 'btn-success' : 'btn-warning';
+        return ($(`<button type="button" class="btn ${btnStyleClass}">Run for ${value}</button>`));
+      }
+    };
+    this.setCustomizedField(runFieldConfig, runFieldName);
+
     return [
-      { name: 'countryName', title: "Country", type: "text", width: 30, validate: "required" },
+      { name: 'countryName', title: "Country", type: "text", width: 32 },
       { name: 'tier', title: "Tier", type: "number", width: 20 },
       { name: 'version', title: "Version", type: "text", width: 30 },
-      { name: 'geosumAccuracy', title: "Geosummarizer Accuracy", type: "number", width: 100 },
-      { name: 'geosumRate', title: "Good Geosummarizer Rate", type: "number", width: 100 },
-      { name: 'inputRate', title: "Ideal Input Rate", type: "number", width: 80 },
-      { name: 'inputSD', title: "Ideal Input Standard Deviation", type: "number", width: 120 },
-      { name: 'inputAccuracy', title: "Input Accuracy", type: "number", width: 80 },
+      { name: 'geosumAccuracy', title: "Geosummarizer Accuracy", type: "number", width: 80 },
+      { name: 'geosumRate', title: "Good Geosummarizer Rate", type: "number", width: 85 },
+      { name: 'inputRate', title: "Ideal Input Rate", type: "number", width: 55 },
+      { name: 'inputSD', title: "Ideal Input Standard Deviation", type: "number", width: 90 },
+      { name: 'inputAccuracy', title: "Input Accuracy", type: "number", width: 50 },
+      { name: 'liveVersion', title: "Run", type: runFieldName, width: 30 }
     ];
   }
 
