93acd516c2c8c6c735f3dff46143ff8b587f5386
Chen Fan <fanchen@Chens-MacBook-Pro.local>
2016-07-22 11:20:48 +0800

    Implement chart data front

--- views/chart-loader/app.js
+++ views/chart-loader/app.js
@@ -1,39 +1,75 @@
 function loadPage() {
-  var ctx = $("#chartBody");
-  var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: ["3.41", "3.42","3.43"],
-      datasets: [{
-        label: 'Density',
-        data: [0.948, 0.936, 0.939],
-        backgroundColor: 'rgba(0,0,0,0.0)',
-        borderColor: 'rgba(255,99,132,1)',
-        borderWidth: 1
-      },
-      {
-        label: 'Accuracy',
-        data: [0.7785, 0.7848, 0.7859],
-        backgroundColor: 'rgba(0,0,0,0.0)',
-        borderColor: 'rgba(54, 162, 235, 1)',
-        borderWidth: 1
-      },
-      {
-        label: 'Comprehensiveness',
-        data: [0.9864, 0.9805, 0.9807],
-        backgroundColor: 'rgba(0,0,0,0.0)',
-        borderColor: 'rgba(255, 206, 86, 1)',
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: false
-          }
-        }]
-      }
-    }
+  loadData((data) => {
+    data = JSON.parse(data);
+    console.log(data);
+    loadChart(data);
   });
 }
+
+function getLabels(records) {
+  return records.map((item) => {
+    return item.version;
+  });
+}
+
+function getDensityDataset(records) {
+  let label = 'Density';
+  let backgroundColor = 'rgba(0,0,0,0.0)';
+  let borderColor = 'rgba(255,99,132,1)';
+  let borderWidth = 2;
+  let data = records.map((item) => {
+    return item.density;
+  });
+  return {label, backgroundColor, borderColor, borderWidth, data};
+}
+
+function getAccuracyDataset(records) {
+  let label = 'Accuracy';
+  let backgroundColor = 'rgba(0,0,0,0.0)';
+  let borderColor = 'rgba(54, 162, 235, 1)';
+  let borderWidth = 2;
+  let data = records.map((item) => {
+    return item.accuracy;
+  });
+  return {label, backgroundColor, borderColor, borderWidth, data};
+}
+
+function getComprehensiveDataset(records) {
+  let label = 'Comprehensiveness';
+  let backgroundColor = 'rgba(0,0,0,0.0)';
+  let borderColor = 'rgba(255, 206, 86, 1)';
+  let borderWidth = 2;
+  let data = records.map((item) => {
+    return item.comprehensiveness;
+  });
+  return {label, backgroundColor, borderColor, borderWidth, data};
+}
+
+function getOption() {
+  return {scales: {yAxes: [{ticks: {beginAtZero: false}}]}};
+}
+
+function loadChart(records) {
+  var ctx = $("#chartBody");
+  var chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: getLabels(records),
+      datasets: [
+        getDensityDataset(records),
+        getAccuracyDataset(records),
+        getComprehensiveDataset(records)
+      ]
+    },
+    options: getOption()
+  });
+}
+
+function loadData(cb) {
+  $.get(getDataUrl(), cb);
+}
+
+function getDataUrl() {
+  return $('#chartStorage').attr('chart-data-url');
+}
+
